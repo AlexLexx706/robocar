97e1bc6d79f5cc0ef90db402eb8dc473225d4b1c
AlexLexx <alexlexx1@gmail.com>
2014-12-04 02:25:45 +0300

    add

---
+++ libraries/period/period.h
@@ -0,0 +1,32 @@
+#ifndef PERIOD_H
+#define PERIOD_H
+//сласс для проверки интервалов
+class Period{
+    unsigned long st;
+    unsigned long duration;
+public:
+    
+    Period(unsigned long duration_mk):duration(duration_mk){st = micros();}
+
+    bool isReady(){
+        //проверка выключена
+        if (duration == 0xffffffff)
+            return false;
+
+        unsigned long ct = micros();
+        unsigned long dt = ct - st;
+        
+        if (dt < duration)
+            return false;
+
+        st = ct - (dt % duration);
+        return true;
+    }
+    
+    void reset() {st = micros();}
+
+    void set_period(unsigned long duration_ms){
+        duration = duration_ms;
+    }
+};
+#endif
